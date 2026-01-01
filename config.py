be142ef5427dbc5bcd627f473576ee62b0df99f3
oid85 <oid85@mail.ru>
2026-01-01 20:11:41 +0700

    .

---
+++ config.py
@@ -0,0 +1,7 @@
+deploy_path = 'C:\\Deploy'
+
+health_source_path = 'C:\\Repository\\Oid85.Health\\Oid85.Athletic'
+health_deploy_directory = 'Oid85.Health'
+health_exe_file_name = "Oid85.Health.WebHost.exe"
+health_service_name = 'Oid85.Health'
+health_appsettings_file = 'appsettings.json'
